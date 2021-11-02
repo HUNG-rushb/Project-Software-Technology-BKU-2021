8bc998bdc880b5f57668ad7ecb6669a628da5669
phuvd1609 <phuvd16092001@gmail.com>
2021-11-02 21:20:59 +0700

    Menu Phú
    thêm menu

---
+++ my_res/src/components/Menu/FoodItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+    <img src="../../assets/beefsteak.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"> Beefsteak </h5>
+      <p class="card-text">Giá: 100000 VNĐ</p>
+      <button class="btn btn-primary center"> Mua </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FoodItem'
+}
+</script>
+
+<style>
+  .center {
+    text-align: center;
+  }
+</style>
---
+++ my_res/src/components/Menu/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row" id="header">
+    <div class="col-lg-6 col-sm-12">
+      <h1>Menu</h1>
+    </div>
+    <div class="col-lg-6 col-sm-12">
+      <h5>Home | Menu | Contact</h5>
+      
+      <div>
+        <label for="search" class="center" id="search_label">Tìm kiếm: </label>
+        <input type="text" name="search_value" class="form-control" id="search">
+        <button class="btn btn-light">Tìm</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: left;
+  padding-left: 22px;
+}
+
+#header {
+    border-bottom: solid black 1px;
+}
+
+h5 {
+    margin-left: 30%;
+    margin-top: 50px;
+}
+
+.center {
+    margin-left: 30%;
+}
+
+/* #search {
+    width: 200px;
+    margin-left: 105px;
+} */
+
+.form-control {
+    width: 25%;
+    display: inline-block;
+    margin-left: 10px;
+}
+
+.btn-light {
+    /* border-color: blue; */
+    margin-left: 10px;
+}
+</style>
