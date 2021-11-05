0128716cc7711f17328c3b0a5dd9a6ef1ef5792e
Your Name <you@example.com>
2021-11-05 18:46:01 +0700

    organize file

--- my_res/src/views/Home.vue
+++ my_res/src/views/Home.vue
@@ -24,7 +24,7 @@ import HeaderVideo from "../components/Home/HeaderVideo.vue";
 import CheckCart from "../components/Layout/CheckCart.vue";
 import AddToast from "../components/Layout/AddToast.vue";
 import HomeMenu from "../components/Home/HomeMenu.vue";
-import GoogleMap from "../components/Layout/GoogleMap.vue";
+import GoogleMap from "../components/Home/GoogleMap.vue";
 import Footer from "../components/Layout/Footer.vue";
 import ReviewFood from "../components/Layout/ReviewFood.vue";
 
