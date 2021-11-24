89c69f5cc344e6392a5fd752eb79abaec9ea0042
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2021-11-24 19:44:16 +0700

    update slider image

--- my_res/src/components/Home/HomeSlider.vue
+++ my_res/src/components/Home/HomeSlider.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="carouselExampleInterval" class="custom carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="3000">
+            <div class="carousel-item active" data-bs-interval="2000">
                 <img src="../../assets/images/Home/slide1.png" class="d-block w-100" alt="...">
             </div>
-            <div class="carousel-item" data-bs-interval="3000">
+            <div class="carousel-item" data-bs-interval="2000">
                 <img src="../../assets/images/Home/slide2.png"  class="d-block w-100" alt="...">
             </div>
-            <div class="carousel-item" data-bs-interval="3000">
+            <div class="carousel-item" data-bs-interval="2000">
                 <img src="../../assets/images/Home/slide3.png" class="d-block w-100" alt="...">
             </div>
         </div>
